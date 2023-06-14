9e531661ffebc86fa8ca7af559809d882128d1f7
Jaynel <lenyajpatiarba@gmail.com>
2023-06-14 15:40:49 +0800

    adding connection

---
+++ authentication/config/services.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+     'paymongo' => ['base_uri' => env('PAYMONGO_SERVICE_BASE_URL')],
+     'gpt' => ['base_uri' => env('GPT_SERVICE_BASE_URL')],
+     'dalle' => ['base_uri' => env('DALLE_SERVICE_BASE_URL')]
+];
